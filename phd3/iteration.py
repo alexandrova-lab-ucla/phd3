7a9904beaa7d75a7f1599f7712165f2ffcb0c492
Matthew Hennefarth <mhennefa@br012.ib.bridges2.psc.edu>
2021-04-30 16:15:59 -0400

    shutil error

--- phd3/iteration.py
+++ phd3/iteration.py
@@ -378,7 +378,7 @@ class iteration:
                 if os.path.isdir("equilibrate"):
                     logger.debug("Copying equilibrate files to new dmd start")
                     for f in [d for d in os.listdir('equilibrate') if os.path.isfile(d)]:
-                        shutul.copy(os.path.join("equilibrate", f), "./")
+                        shutil.copy(os.path.join("equilibrate", f), "./")
 
                     logger.debug("Removing echo and movie file from directory")
                     os.remove(self.parameters["dmd params"]["Echo File"])
