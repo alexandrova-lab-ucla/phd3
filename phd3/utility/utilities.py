f1c9bf0646de1dfc8785aec9dbba07f548056c6c
jtfuller3 <50637037+jtfuller3@users.noreply.github.com>
2021-12-09 12:46:29 -0800

    Update utilities.py
    try with openbabel 2 if openbabel 3 doesn't work

--- phd3/utility/utilities.py
+++ phd3/utility/utilities.py
@@ -606,17 +606,17 @@ def make_mol2(res: residue, reformat: bool=True):
             mol2_file.write(atom.pdb_line())
 
         mol2_file.write('TER\nENDMDL')
-    # Now we execute the babel command here
-    try:
-        with Popen(f"obabel -i pdb {res.name}.pdb -o mol2 -O {res.name}.mol2", stdin=PIPE, stdout=PIPE, stderr=PIPE,
-                   universal_newlines=True, shell=True, bufsize=1, env=os.environ) as shell:
-            while shell.poll() is None:
-                logger.debug(shell.stdout.readline().strip())
-                output = shell.stderr.readline().strip()
-                logger.debug(output)
-                if "1 molecule converted" in output:
-                    successful = True
-    except:
+    # Now we execute the babel command here -- try openbabel 3 first
+    with Popen(f"obabel -i pdb {res.name}.pdb -o mol2 -O {res.name}.mol2", stdin=PIPE, stdout=PIPE, stderr=PIPE,
+               universal_newlines=True, shell=True, bufsize=1, env=os.environ) as shell:
+        while shell.poll() is None:
+            logger.debug(shell.stdout.readline().strip())
+            output = shell.stderr.readline().strip()
+            logger.debug(output)
+            if "1 molecule converted" in output:
+                successful = True
+
+    if not successful:  # try with openbabel 2 syntax
         with Popen(f"babel {res.name}.pdb {res.name}.mol2", stdin=PIPE, stdout=PIPE, stderr=PIPE,
                    universal_newlines=True, shell=True, bufsize=1, env=os.environ) as shell:
             while shell.poll() is None:
